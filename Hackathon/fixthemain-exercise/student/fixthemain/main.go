0918965b8125f53ebe62cc2d5c526b5c72193b8b
iyoussef <ibraheemyoussefiy@gmail.com>
2025-12-26 20:14:40 +0000

    fix: Commented out broken program in fixthemain exercise

--- Hackathon/fixthemain-exercise/student/fixthemain/main.go
+++ Hackathon/fixthemain-exercise/student/fixthemain/main.go
@@ -1,48 +1,46 @@
 package main
 
-import "github.com/01-edu/z01"
-
 // TODO: Fix all the errors in this program
 
 // Missing: Define OPEN and CLOSE constants
 
 // Missing: Define Door struct with state field
 
-func PrintStr(s string) {
-	for _, r := range s {
-		z01.PrintRune(r)
-	}
-	// Missing: Add newline
-}
+// func PrintStr(s string) {
+// 	for _, r := range s {
+// 		z01.PrintRune(r)
+// 	}
+// 	// Missing: Add newline
+// }
 
-// Missing: Define OpenDoor function
+// // Missing: Define OpenDoor function
 
-func CloseDoor(ptrDoor *Door) {
-	PrintStr("Door Closing...")
-	ptrDoor.state = CLOSE // Fixed: was "state = CLOSE"
-}
+// func CloseDoor(ptrDoor *Door) {
+// 	PrintStr("Door Closing...")
+// 	ptrDoor.state = CLOSE // Fixed: was "state = CLOSE"
+// }
 
-func IsDoorOpen(ptrDoor *Door) bool { // Fixed: parameter type
-	PrintStr("is the Door opened ?")
-	return ptrDoor.state == OPEN // Fixed: was "Door.state = OPEN"
-}
+// func IsDoorOpen(ptrDoor *Door) bool { // Fixed: parameter type
+// 	PrintStr("is the Door opened ?")
+// 	return ptrDoor.state == OPEN // Fixed: was "Door.state = OPEN"
+// }
 
-func IsDoorClose(ptrDoor *Door) bool {
-	PrintStr("is the Door closed ?")
-	return ptrDoor.state == CLOSE // Fixed: added return statement
-}
+// func IsDoorClose(ptrDoor *Door) bool {
+// 	PrintStr("is the Door closed ?")
+// 	return ptrDoor.state == CLOSE // Fixed: added return statement
+// }
 
-func main() {
-	door := &Door{}
+// func main() {
+// 	door := &Door{}
 
-	OpenDoor(door)
-	if IsDoorClose(door) {
-		OpenDoor(door)
-	}
-	if IsDoorOpen(door) {
-		CloseDoor(door)
-	}
-	if door.state == OPEN {
-		CloseDoor(door)
-	}
-}
+// 	OpenDoor(door)
+// 	if IsDoorClose(door) {
+// 		OpenDoor(door)
+// 	}
+// 	if IsDoorOpen(door) {
+// 		CloseDoor(door)
+// 	}
+// 	if door.state == OPEN {
+// 		CloseDoor(door)
+// 	}
+// }
